2c8392e88f3f4f2b42b18698e2eeae6bb5dc9644
GDL486 <44408694+GDL486@users.noreply.github.com>
2018-11-19 19:16:16 +0100

    Update alphabeta.py

--- alphabeta.py
+++ alphabeta.py
@@ -1,4 +1,3 @@
-# Complete this class for all parts of the project
 
 from pacman_module.game import Agent
 from pacman_module.pacman import Directions
@@ -12,6 +11,99 @@ class PacmanAgent(Agent):
         - `args`: Namespace of arguments from command-line prompt.
         """
         self.args = args
+        self.dictExpanded = {}
+        self.move = None
+        
+    def hashPosFood(self, state):
+        """
+        Arguments:
+        ----------
+        - `state`: a game state see `pacman.GameState`.
+
+        Return:
+        -------
+
+        -`key`: a string that is made with the position of
+        pacman hashed and the grid of position
+        of foods (getFood) hashed.
+
+        Note:
+        -----
+        Only the two data of the state mentioned above are taken
+        into account in the hash function.
+
+        """
+        hash1 = hash(state.getPacmanPosition())
+        hash2 = hash(state.getFood())
+        posGhost = state.getGhostPositions()[0]
+        hash3 = hash((posGhost[0],posGhost[1]))
+        key = str(hash1) + ' ' + str(hash2) + ' ' + str(hash3)
+        return key
+    
+    def isBestDepth(self,state, depth):
+        key = self.hashPosFood(state)
+        
+        pacmanPos = state.getPacmanPosition()
+        food = state.getFood()
+        ghostPos = state.getGhostPositions()
+        
+        stateComponent = (pacmanPos, food, ghostPos)
+        if key not in self.dictExpanded:
+            self.dictExpanded[key] = [[stateComponent,depth]]
+            return True
+        else :
+            for element in self.dictExpanded[key]:
+                if element[0] == stateComponent :
+                    if depth < element[1]:
+                        element[1] = depth
+                        return True
+                    return False
+            self.dictExpanded[key].append([stateComponent,depth])
+            return True
+            
+            
+    def minimax(self, node, depth, player, alpha, beta):
+        if node.isWin() or node.isLose():
+            
+            return node.getScore()
+        
+        
+        # Maximize function (Pacman)
+        if player:
+            value = float("-inf")
+            for successor in node.generatePacmanSuccessors():
+                if  self.isBestDepth(successor[0],depth): 
+                    successor_value = self.minimax(successor[0], depth +1, 0, alpha, beta) 
+                    
+                    if successor_value > value:
+                        value = successor_value
+                        
+                        if depth == 0:
+                            self.move = successor[1]
+                        if value >= beta:
+                            return value
+                        alpha = max (alpha,value)
+                        
+                        
+           
+            return value
+        
+        # Minimize function (Ghost)
+        else:
+            value = float("inf")
+            
+            for successor in node.generateGhostSuccessors(1):
+                if self.isBestDepth(successor[0],depth) :
+                    successor_value = self.minimax(successor[0], depth + 1, 1, alpha, beta)
+                    
+                    if successor_value < value:
+                        value = successor_value
+                       
+                        if value <= alpha:
+                            return value
+                        beta = min(beta,value)
+                        
+            return value
 
     def get_action(self, state):
         """
@@ -26,4 +118,6 @@ class PacmanAgent(Agent):
         -------
         - A legal move as defined in `game.Directions`.
         """
-        return Directions.STOP
+        self.minimax(state, 0, 1,float("-inf"),float("inf"))
+        
+        return self.move
