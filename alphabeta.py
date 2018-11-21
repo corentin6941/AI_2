7fd02b584652caf3f8bb26cb986eb6f03fdd9f8d
GDL486 <44408694+GDL486@users.noreply.github.com>
2018-11-21 21:37:06 +0100

    Update alphabeta.py

--- alphabeta.py
+++ alphabeta.py
@@ -40,6 +40,26 @@ class PacmanAgent(Agent):
         key = str(hash1) + ' ' + str(hash2) + ' ' + str(hash3)
         return key
     
+    def keysInDict(self,state):
+        key = self.hashPosFood(state)
+        
+        if key not in self.dictExpanded :
+            return None
+        pacmanPos = state.getPacmanPosition()
+        food = state.getFood()
+        ghostPos = state.getGhostPositions()
+        
+        stateComponent = (pacmanPos, food, ghostPos)
+        
+        listInDict =self.dictExpanded[key]
+        
+       
+        
+        for i in range(len(listInDict)):
+            if listInDict[i][0] == stateComponent :
+                return [key,i]
+        return None
+    
     def isBestDepth(self,state, depth):
         key = self.hashPosFood(state)
         
@@ -48,6 +68,7 @@ class PacmanAgent(Agent):
         ghostPos = state.getGhostPositions()
         
         stateComponent = (pacmanPos, food, ghostPos)
+
         if key not in self.dictExpanded:
             self.dictExpanded[key] = [[stateComponent,depth]]
             return True
@@ -62,7 +83,7 @@ class PacmanAgent(Agent):
             return True
             
             
-    def minimax(self, node, depth, player, alpha, beta):
+    def minimax(self, node, depth, player,alpha,beta):
         if node.isWin() or node.isLose():
             
             return node.getScore()
@@ -71,38 +92,58 @@ class PacmanAgent(Agent):
         # Maximize function (Pacman)
         if player:
             value = float("-inf")
+            key = self.hashPosFood(node)
+            hasNoneChild = True
             for successor in node.generatePacmanSuccessors():
                 if  self.isBestDepth(successor[0],depth): 
-                    successor_value = self.minimax(successor[0], depth +1, 0, alpha, beta) 
+                    
+                    successor_value = self.minimax(successor[0], depth +1, 0,alpha,beta)
+                    
+                    if not successor_value :
+                        continue
                     
                     if successor_value > value:
+                        hasNoneChild = False
                         value = successor_value
-                        
-                        if depth == 0:
-                            self.move = successor[1]
                         if value >= beta:
                             return value
                         alpha = max (alpha,value)
                         
+                        if depth == 0:
+                            self.move = successor[1]
                         
-           
+                        else :
+                            [key,index]= self.keysInDict(node)
+                            if len(self.dictExpanded[key][index])==2:
+                                self.dictExpanded[key][index].append(successor[1])
+                        
+                            if len(self.dictExpanded[key][index])==3:
+                                self.dictExpanded[key][index][2] = successor[1]
+            if hasNoneChild:
+                return None
             return value
-        
         # Minimize function (Ghost)
         else:
             value = float("inf")
-            
+            hasNoneChild = True
             for successor in node.generateGhostSuccessors(1):
+                
                 if self.isBestDepth(successor[0],depth) :
-                    successor_value = self.minimax(successor[0], depth + 1, 1, alpha, beta)
+                    successor_value = self.minimax(successor[0], depth + 1, 1,alpha,beta)
                     
+                    if not successor_value :
+                        continue
                     if successor_value < value:
+                        hasNoneChild = False
                         value = successor_value
-                       
+                        if depth == 0:
+                            self.move = successor[1]
+                            
                         if value <= alpha:
                             return value
                         beta = min(beta,value)
-                        
+            if hasNoneChild:
+                return None
             return value
 
     def get_action(self, state):
@@ -118,6 +159,16 @@ class PacmanAgent(Agent):
         -------
         - A legal move as defined in `game.Directions`.
         """
-        self.minimax(state, 0, 1,float("-inf"),float("inf"))
+        if not self.move:
+            self.minimax(state, 0, 1,float("-inf"),float("inf"))
+
+        else:
+            if not self.keysInDict(state):
+                self.dictExpanded = {}
+                self.minimax(state, 0, 1,float("-inf"),float("inf"))
+            else :
+                [key,index] =self.keysInDict(state)
+            self.move =self.dictExpanded[key][index][2]
+            
         
         return self.move
